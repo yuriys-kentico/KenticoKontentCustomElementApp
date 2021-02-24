a76aa86a5f39c37e30e32825a2f28dd9c8d4f1ed
Yuriy Sountsov <yuriys@kentico.com>
2021-02-24 11:09:01 -0500

    Property action only if value defined

--- template/src/shared/actions/property.ts
+++ template/src/shared/actions/property.ts
@@ -1,6 +1,24 @@
-import { is_function } from 'svelte/internal';
+import { is_function } from "svelte/internal";
 
 type ActionType<T = any> = [string, T] | [string, () => T];
+
+export const property = (node: HTMLElement, target: ActionType) => {
+  setProperty(target, node);
+
+  return {
+    update: (target: ActionType) => {
+      setProperty(target, node);
+    },
+  };
+};
+
+function setProperty(target: ActionType<any>, node: HTMLElement) {
+  let [name, value] = extractValue(target);
+
+  if (value !== undefined) {
+    node.style.setProperty(`--${name}`, String(value));
+  }
+}
 
 const extractValue = (target: ActionType) => {
   let [name, value] = target;
@@ -10,18 +28,4 @@ const extractValue = (target: ActionType) => {
   }
 
   return [name, value];
-};
-
-export const property = (node: HTMLElement, target: ActionType) => {
-  let [name, value] = extractValue(target);
-
-  node.style.setProperty(`--${name}`, String(value));
-
-  return {
-    update: (target: ActionType) => {
-      let [name, value] = extractValue(target);
-
-      node.style.setProperty(`--${name}`, String(value));
-    },
-  };
 };
